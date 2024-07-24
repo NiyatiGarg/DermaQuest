084520a4311f69a5256d95f57d50d3e20517f9b1
Niyati Garg <niyatigarg2002@gmail.com>
2024-07-24 13:04:16 +0530

    created basic structure

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,20 +1,52 @@
-import React , {useContext} from 'react';
+import React , {useContext, useState} from 'react';
 
 import { AppContext } from '../AppContext';
 import Header from './Header';
 
+import Icofont from 'react-icofont';
+
 function Blogs() {
 
-  const { bgColor } = useContext(AppContext);
+  const { pageTheme } = useContext(AppContext);
+  const [inputValue, setInputValue]= useState('')
+  
+  const handleInputChange=(e)=>{
+    setInputValue(e.target.value);
+  }
 
   return (
         <div >
         <Header />
-        <div className='d-flex justify-content-center'>
-          <h1>
-           Frequently asked topics
-          </h1>
+        <section className='justify-content-center d-flex flex-column' style={{padding: '2vh 10vw'}}>
+        <h1>Frequently asked topics</h1>
+ 
+      <div
+          style={{
+            width: '100%',
+            backgroundColor: pageTheme,
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+            // borderRadius: 
+
+          }} className='d-flex mt-4'>
+            
+          <input
+            autofocus={false}
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Ask about ingredients..."
+            className="form-control "
+            style={{
+              backgroundColor: 'transparent',
+              border: 'none',
+              paddingRight: '30px',
+              color: 'black',
+              
+            }}
+          />
+          <Icofont icon="search" className='p-2' style={{ display: 'flex', fontSize: '2rem' }} />
         </div>
+      </section>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../AppContext';
+
+import Icofont from 'react-icofont';
 
 import Header from './Header';
 
 function Home() {
 
-  const { setPageTheme } = useContext(AppContext);
+  const { pageTheme } = useContext(AppContext);
+const [inputValue, setInputValue]= useState('')
+
+const handleInputChange=(e)=>{
+  setInputValue(e.target.value);
+}
 
   return (
     <div >
       <Header />
-      <div className='justify-content-center d-flex'>
-        <h1>This is the Home page </h1>
-      </div>
+      <section className='justify-content-center d-flex' style={{padding: '2vh 10vw'}}>
+      <div
+          style={{
+            width: '100%',
+            backgroundColor: pageTheme,
+            boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+            // borderRadius: 
+
+          }} className='d-flex mt-4'>
+          <input
+            autofocus={false}
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Ask about ingredients, routines, and more..."
+            className="form-control "
+            style={{
+              backgroundColor: 'transparent',
+              border: 'none',
+              paddingRight: '30px',
+              color: 'black',
+              
+            }}
+          />
+          <Icofont icon="search" className='p-2' style={{ display: 'flex', fontSize: '2rem' }} />
+        </div>
+      </section>
     </div>
   )
 }
