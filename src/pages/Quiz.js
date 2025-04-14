5eb2a0c7d9806dd2c2b00e489371fa9f6e225347
apple <apple@MacBook-Air.local>
2025-04-14 14:51:18 +0530

    df

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -5,14 +5,14 @@ import Header from './Header';
 
 function Quiz() {
 
-  const { bgColor } = useContext(AppContext);
+  // const { bgColor } = useContext(AppContext);
 
   return (
     <>
         <Header />
         <div className='d-flex justify-content-center align-items-center' >
           <h1 className='p-5'>
-            Coming Soon
+            Coming Soon...
           </h1>
         </div>
     </>
